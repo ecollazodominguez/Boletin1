bdc5f84ab8fd3e40283d52d6aabc0640ab7a6ddb
ecollazodominguez <ecollazodominguez@nautilus9.danielcastelao.org>
2018-10-09 09:13:43 +0200

    inicio ejercicio2

---
+++ Boletin1_2/src/boletin1_2/Boletin1_2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_2;
+
+/**
+ *
+ * @author ecollazodominguez
+ */
+public class Boletin1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     //calculamos area de cuadrado
+        int lado= 3;
+        System.out.println("Área cuadrado = "+lado*lado);
+        
+    }
+    
+}
