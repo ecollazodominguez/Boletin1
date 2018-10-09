47890ffc0499c5d239dee6bdd6e1d58fde5e1323
ecollazodominguez <ecollazodominguez@nautilus9.danielcastelao.org>
2018-10-09 09:12:28 +0200

    inicio ejercicio1

---
+++ Boletin1_1/src/boletin1_1/Boletin1_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_1;
+
+/**
+ *
+ * @author ecollazodominguez
+ */
+public class Boletin1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //calculamos area de triángulo
+        int base= 4, altura= 3;
+        System.out.println("Area triángulo= "+(base*altura)/2);
+    }
+    
+}
